e2ad75fe6ba19d558250ca82f7f5c1115072f9ec
mmuller <mmuller@enduden.com>
2010-12-26 17:45:45 -0500

    Added a bunch of random stuff to GL, mostly to support textures and rotation.

---
+++ ext/RCObj.h
@@ -0,0 +1,33 @@
+// Copyright 2010 Google Inc.
+// XXX do we really need 3 reference counting mechanisms???
+
+#ifndef _crack_ext_RCObj_h
+#define _crack_ext_RCObj_h
+
+namespace crack { namespace ext {
+
+class RCObj {
+    protected:
+        unsigned int refCount;
+
+    public:
+        RCObj() : refCount(1) {}
+        virtual ~RCObj();
+
+        /**
+         * Increments the reference count of the object.
+         */
+        void bind();
+        
+        /**
+         * Decrements the reference count of the object, deleting it if it 
+         * drops to zero.
+         */
+        void release();
+};
+
+}}
+
+#endif
+
+
