10db2a9a7b65ac8c922f556c34df866309ee430a
Conrad Steenberg <conrad.steenberg@caltech.edu>
2011-01-02 22:00:57 -0800

    Pull changes from math module.

---
+++ compiler/Token2.cc
@@ -0,0 +1,114 @@
+// Copyright 2010 Google Inc.
+
+#include "Token.h"
+
+#include "parser/Token.h"
+#include "Location.h"
+
+using namespace compiler;
+
+Token::Token(const parser::Token &tok) : 
+    rep(new parser::Token(tok)),
+    loc(0) {
+}
+
+Token::Token(int type, const char *text, Location *loc) : loc(loc) {
+    rep = new parser::Token(static_cast<parser::Token::Type>(type), text, 
+                            *loc->rep
+                            );
+    loc->bind();
+}
+
+Token *Token::create(int type, const char *text, Location *loc) {
+    return new Token(type, text, loc);
+}
+
+Token::~Token() {
+    delete rep;
+    if (loc)
+        loc->release();
+}
+
+bool Token::hasText(const char *text) {
+    return rep->getData() == text;
+}
+
+const char *Token::getText() {
+    return rep->getData().c_str();
+}
+
+int Token::getType() {
+    return rep->getType();
+}
+
+size_t Token::getTextSize() {
+    return rep->getData().size();
+}
+
+Location *Token::getLocation() {
+    if (!loc) {
+        loc = new Location(rep->getLocation());
+        loc->bind();
+    }
+    return loc;
+}
+
+bool Token::isAnn() { return rep->isAnn(); }
+bool Token::isBoolAnd() { return rep->isBoolAnd(); }
+bool Token::isBoolOr() { return rep->isBoolOr(); }
+bool Token::isIf() { return rep->isIf(); }
+bool Token::isIn() { return rep->isIn(); }
+bool Token::isImport() { return rep->isImport(); }
+bool Token::isElse() { return rep->isElse(); }
+bool Token::isOper() { return rep->isOper(); }
+bool Token::isOn() { return rep->isOn(); }
+bool Token::isWhile() { return rep->isWhile(); }
+bool Token::isReturn() { return rep->isReturn(); }
+bool Token::isBreak() { return rep->isBreak(); }
+bool Token::isClass() { return rep->isClass(); }
+bool Token::isContinue() { return rep->isContinue(); }
+bool Token::isDollar() { return rep->isDollar(); }
+bool Token::isNull() { return rep->isNull(); }
+bool Token::isIdent() { return rep->isIdent(); }
+bool Token::isString() { return rep->isString(); }
+bool Token::isIstrBegin() { return rep->isIstrBegin(); }
+bool Token::isIstrEnd() { return rep->isIstrEnd(); }
+bool Token::isSemi() { return rep->isSemi(); }
+bool Token::isComma() { return rep->isComma(); }
+bool Token::isColon() { return rep->isColon(); }
+bool Token::isDecr() { return rep->isDecr(); }
+bool Token::isDefine() { return rep->isDefine(); }
+bool Token::isDot() { return rep->isDot(); }
+bool Token::isIncr() { return rep->isIncr(); }
+bool Token::isAssign() { return rep->isAssign(); }
+bool Token::isLParen() { return rep->isLParen(); }
+bool Token::isRParen() { return rep->isRParen(); }
+bool Token::isLCurly() { return rep->isLCurly(); }
+bool Token::isRCurly() { return rep->isRCurly(); }
+bool Token::isLBracket() { return rep->isLBracket(); }
+bool Token::isRBracket() { return rep->isRBracket(); }
+bool Token::isInteger() { return rep->isInteger(); }
+bool Token::isFloat() { return rep->isFloat(); }
+bool Token::isOctal() { return rep->isOctal(); }
+bool Token::isHex() { return rep->isHex(); }
+bool Token::isBinary() { return rep->isBinary(); }
+bool Token::isPlus() { return rep->isPlus(); }
+bool Token::isQuest() { return rep->isQuest(); }
+bool Token::isMinus() { return rep->isMinus(); }
+bool Token::isAsterisk() { return rep->isAsterisk(); }
+bool Token::isBang() { return rep->isBang(); }
+bool Token::isSlash() { return rep->isSlash(); }
+bool Token::isPercent() { return rep->isPercent(); }
+bool Token::isNot() { return rep->isNot(); }
+bool Token::isTilde() { return rep->isTilde(); }
+bool Token::isGT() { return rep->isGT(); }
+bool Token::isLT() { return rep->isLT(); }
+bool Token::isEQ() { return rep->isEQ(); }
+bool Token::isNE() { return rep->isNE(); }
+bool Token::isGE() { return rep->isGE(); }
+bool Token::isLE() { return rep->isLE(); }
+bool Token::isEnd() { return rep->isEnd(); }
+bool Token::isLogicAnd() { return rep->isLogicAnd(); }
+bool Token::isLogicOr() { return rep->isLogicOr(); }
+bool Token::isBinOp() { return rep->isBinOp(); }
+bool Token::isAugAssign() { return rep->isAugAssign(); }
---
+++ test/testext.cc
@@ -0,0 +1,80 @@
+
+#include <string.h>
+#include "ext/Func.h"
+#include "ext/Module.h"
+#include "ext/Type.h"
+#include "ext/Object.h"
+
+#include <iostream>
+
+using namespace crack::ext;
+using namespace std;
+
+class MyType {
+    public:
+        int a;
+        const char *b;
+        
+        MyType(int a, const char *b) : a(a), b(b) {}
+
+        static MyType *oper_new() {
+            return new MyType(100, "test");
+        }
+
+        const char *echo(const char *data) { return data; }
+};
+
+class MyAggType : public Object {
+    public:
+        int a;
+
+        void init(int a0) { a = a0; }
+        void dump() { 
+            cout << "a = " << a << endl; 
+            char *c = (char *)this;
+            for (int i = 0; i < 32; ++i)
+                cout << hex << ((int)c[i] & 0xff) << " ";
+            cout << endl;
+        }
+};    
+
+const char *echo(const char *data) { return data; }
+
+int *copyArray(int count, int *array) {
+    int *result = new int[count];
+    return (int *)memcpy(result, array, count * sizeof(int));
+}
+
+extern "C" void test_testext_init(Module *mod) {
+    Func *f = mod->addFunc(mod->getByteptrType(), "echo", (void *)echo);
+    f->addArg(mod->getByteptrType(), "data");
+    
+    Type *type = mod->addType("MyType");
+    type->addInstVar(mod->getIntType(), "a");
+    type->addInstVar(mod->getByteptrType(), "b");
+
+    type->addStaticMethod(type, "oper new", (void *)MyType::oper_new);
+    f = type->addMethod(mod->getByteptrType(), "echo", (void *)&MyType::echo);
+    f->addArg(mod->getByteptrType(), "data");
+    type->finish();
+    
+    type = mod->addType("MyAggType");
+    type->addBase(mod->getObjectType());
+    type->addInstVar(mod->getIntType(), "a"); 
+    f = type->addConstructor();
+    f = type->addConstructor("init", (void *)&MyAggType::init);
+    f->addArg(mod->getIntType(), "a");
+    type->addMethod(mod->getVoidType(), "dump", (void *)&MyAggType::dump);
+    type->finish();
+
+    mod->addConstant(mod->getIntType(), "INT_CONST", 123);
+    mod->addConstant(mod->getFloatType(), "FLOAT_CONST", 1.23);
+
+    Type *arrayType = mod->getType("array");
+    vector<Type *> params(1);
+    params[0] = mod->getIntType();
+    Type *intArrayType = arrayType->getSpecialization(params);
+    f = mod->addFunc(intArrayType, "copyArray", (void *)copyArray);
+    f->addArg(mod->getIntType(), "count");
+    f->addArg(intArrayType, "array");
+}
