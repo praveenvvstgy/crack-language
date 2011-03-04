2678d7f91399c07ff25964d4089b498046f20e34
weyrick <weyrick@mozek.us>
2011-03-04 16:59:04 -0500

    LLVM 2.9 branch. DebugInfo is disabled currently, pending 2.9 debug changes. Otherwise full test suite passes.

---
+++ builder/llvm/BBuilderContextData.h
@@ -0,0 +1,82 @@
+// Copyright 2010 Google Inc, Shannon Weyrick <weyrick@mozek.us>
+
+#ifndef _builder_llvm_BBuilderContextData_h_
+#define _builder_llvm_BBuilderContextData_h_
+
+#include <vector>
+#include "model/BuilderContextData.h"
+#include "model/Context.h"
+#include <spug/RCPtr.h>
+
+namespace llvm {
+    class Function;
+    class BasicBlock;
+    class SwitchInst;
+    class Value;
+}
+
+namespace builder { namespace mvll {
+
+class IncompleteCatchSelector;
+
+SPUG_RCPTR(BBuilderContextData);
+
+class BBuilderContextData : public model::BuilderContextData {
+public:
+    // state information on the current try/catch block.
+    struct CatchData {
+        // list of the implementation values of the classes in "catch" clauses
+        std::vector<llvm::Value *> classImpls;
+        
+        // list of the incomplete catch selectors for the block.
+        std::vector<IncompleteCatchSelector *> selectors;
+        
+        // the switch instruction for the catch blocks
+        llvm::SwitchInst *switchInst;
+    };
+
+    llvm::Function *func;
+    llvm::BasicBlock *block,
+        *unwindBlock,      // the unwind block for the catch/function
+        *nextCleanupBlock; // the current next cleanup block
+
+    BBuilderContextData() :
+            func(0),
+            block(0),
+            unwindBlock(0),
+            nextCleanupBlock(0),
+            catchData(0) {
+    }
+    
+    ~BBuilderContextData() {
+        delete catchData;
+    }
+
+    static BBuilderContextData *get(model::Context *context) {
+        if (!context->builderData)
+            context->builderData = new BBuilderContextData();
+        return BBuilderContextDataPtr::rcast(context->builderData);
+    }
+    
+    CatchData &getCatchData() {
+        if (!catchData)
+            catchData = new CatchData();
+        return *catchData;
+    }
+    
+    void deleteCatchData() {
+        assert(catchData);
+        delete catchData;
+        catchData = 0;
+    }
+
+    llvm::BasicBlock *getUnwindBlock(llvm::Function *func);
+
+private:
+    CatchData *catchData;
+    BBuilderContextData(const BBuilderContextData &);
+};
+
+}} // end namespace builder::vmll
+
+#endif
---
+++ builder/llvm/BTypeDef.h
@@ -0,0 +1,96 @@
+// Copyright 2010 Google Inc, Shannon Weyrick <weyrick@mozek.us>
+
+#ifndef _builder_llvm_BTypeDef_h_
+#define _builder_llvm_BTypeDef_h_
+
+#include "model/TypeDef.h"
+#include "VTableBuilder.h"
+#include <llvm/Type.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace llvm {
+    class Type;
+    class Constant;
+    class GlobalVariable;
+}
+
+namespace builder {
+namespace mvll {
+
+class PlaceholderInstruction;
+
+SPUG_RCPTR(BTypeDef);
+
+// (RCPTR defined above)
+class BTypeDef : public model::TypeDef {
+public:
+    unsigned fieldCount;
+    llvm::GlobalVariable *classInst;
+    const llvm::Type *rep;
+    unsigned nextVTableSlot;
+    std::vector<PlaceholderInstruction *> placeholders;
+
+    // mapping from base types to their vtables.
+    std::map<BTypeDef *, llvm::Constant *> vtables;
+    llvm::PATypeHolder firstVTableType;
+
+    BTypeDef(TypeDef *metaType, const std::string &name,
+             const llvm::Type *rep,
+             bool pointer = false,
+             unsigned nextVTableSlot = 0
+             ) :
+        model::TypeDef(metaType, name, pointer),
+        fieldCount(0),
+        classInst(0),
+        rep(rep),
+        nextVTableSlot(nextVTableSlot),
+        firstVTableType(0) {
+    }
+
+    // add all of my virtual functions to 'vtb'
+    void extendVTables(VTableBuilder &vtb);
+
+    /**
+     * Create all of the vtables for 'type'.
+     * 
+     * @param vtb the vtable builder
+     * @param name the name stem for the VTable global variables.
+     * @param vtableBaseType the global vtable base type.
+     * @param firstVTable if true, we have not yet discovered the first 
+     *  vtable in the class schema.
+     */
+    void createAllVTables(VTableBuilder &vtb, const std::string &name,
+                          BTypeDef *vtableBaseType,
+                          bool firstVTable = true
+                          );
+
+    /**
+     * Add a base class to the type.
+     */
+    void addBaseClass(BTypeDef *base);
+    
+    /**
+     * register a placeholder instruction to be replaced when the class is 
+     * completed.
+     */
+    void addPlaceholder(PlaceholderInstruction *inst);
+    
+    /**
+     * Find the ancestor with our first vtable.
+     */
+    BTypeDef *findFirstVTable(BTypeDef *vtableBaseType);
+    
+    /**
+     * Get the global variable, creating an extern instance in the module if 
+     * it lives in another module.
+     */
+    llvm::GlobalVariable *getClassInstRep(llvm::Module *module);
+};
+
+} // end namespace builder::vmll
+} // end namespace builder
+
+#endif
