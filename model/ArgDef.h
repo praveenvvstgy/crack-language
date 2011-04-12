494fc791adbaa1215ae30af4ac6022b617db0b38
weyrick <weyrick@mozek.us>
2011-04-11 21:32:43 -0400

    Use new PathV2 from LLVM 2.9, fix deprecation warnings

---
+++ model/ArgDef.h
@@ -0,0 +1,23 @@
+// Copyright 2009 Google Inc.
+
+#ifndef _model_ArgDef_h_
+#define _model_ArgDef_h_
+
+#include "VarDef.h"
+
+namespace model {
+
+SPUG_RCPTR(TypeDef);
+
+SPUG_RCPTR(ArgDef);
+
+class ArgDef : public VarDef {
+    public:
+        ArgDef(TypeDef *type, const std::string &name) :
+            VarDef(type, name) {
+        }
+};
+
+} // namespace model
+
+#endif
