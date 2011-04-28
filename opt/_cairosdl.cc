c1e2f7b5cebca01f05515b56b2690d78fd923934
Conrad Steenberg <conrad.steenberg@caltech.edu>
2011-04-28 11:37:14 -0700

    Regenerate _cairosdl.cc.

--- opt/_cairosdl.cc
+++ opt/_cairosdl.cc
@@ -11,11 +11,35 @@ extern "C"
 void crack_ext__cairosdl_init(crack::ext::Module *mod) {
     crack::ext::Func *f;
     crack::ext::Type *type_Class = mod->getClassType();
-    crack::ext::Type *type_SDL_Rect = mod->addType("SDL_Rect");
-    type_SDL_Rect->finish();
+    crack::ext::Type *type_void = mod->getVoidType();
+    crack::ext::Type *type_voidptr = mod->getVoidptrType();
+    crack::ext::Type *type_bool = mod->getBoolType();
+    crack::ext::Type *type_byteptr = mod->getByteptrType();
+    crack::ext::Type *type_byte = mod->getByteType();
+    crack::ext::Type *type_int32 = mod->getInt32Type();
+    crack::ext::Type *type_int64 = mod->getInt64Type();
+    crack::ext::Type *type_uint32 = mod->getUint32Type();
+    crack::ext::Type *type_uint64 = mod->getUint64Type();
+    crack::ext::Type *type_int = mod->getIntType();
+    crack::ext::Type *type_uint = mod->getUintType();
+    crack::ext::Type *type_float32 = mod->getFloat32Type();
+    crack::ext::Type *type_float64 = mod->getFloat64Type();
+    crack::ext::Type *type_float = mod->getFloatType();
+
+    crack::ext::Type *type_cairo_t = mod->addType("cairo_t");
+    type_cairo_t->finish();
+
+
+    crack::ext::Type *type_cairo_surface_t = mod->addType("cairo_surface_t");
+    type_cairo_surface_t->finish();
+
 
     crack::ext::Type *type_SDL_Surface = mod->addType("SDL_Surface");
     type_SDL_Surface->finish();
+
+
+    crack::ext::Type *type_SDL_Rect = mod->addType("SDL_Rect");
+    type_SDL_Rect->finish();
 
 
     crack::ext::Type *array = mod->getType("array");
@@ -26,98 +50,78 @@ void crack_ext__cairosdl_init(crack::ext::Module *mod) {
         params[0] = type_SDL_Rect;
         array_pSDL__Rect_q = array->getSpecialization(params);
     }
-    crack::ext::Type *type_bool = mod->getBoolType();
-    crack::ext::Type *type_byte = mod->getByteType();
-    crack::ext::Type *type_byteptr = mod->getByteptrType();
-    crack::ext::Type *type_cairo_surface_t = mod->addType("cairo_surface_t");
-    type_cairo_surface_t->finish();
-
-    crack::ext::Type *type_cairo_t = mod->addType("cairo_t");
-    type_cairo_t->finish();
-
-    crack::ext::Type *type_float = mod->getFloatType();
-    crack::ext::Type *type_float32 = mod->getFloat32Type();
-    crack::ext::Type *type_float64 = mod->getFloat64Type();
-    crack::ext::Type *type_int = mod->getIntType();
-    crack::ext::Type *type_int32 = mod->getInt32Type();
-    crack::ext::Type *type_int64 = mod->getInt64Type();
-    crack::ext::Type *type_uint = mod->getUintType();
-    crack::ext::Type *type_uint32 = mod->getUint32Type();
-    crack::ext::Type *type_uint64 = mod->getUint64Type();
-    crack::ext::Type *type_void = mod->getVoidType();
-    crack::ext::Type *type_voidptr = mod->getVoidptrType();
-
     f = mod->addFunc(type_cairo_surface_t, "cairosdl_surface_create",
                      (void *)cairosdl_surface_create
                      );
-    f->addArg(type_SDL_Surface, "sdl_surface");
+       f->addArg(type_SDL_Surface, "sdl_surface");
 
     f = mod->addFunc(type_voidptr, "cairosdl_surface_create_void",
                      (void *)cairosdl_surface_create_void
                      );
-    f->addArg(type_voidptr, "sdl_surface");
+       f->addArg(type_voidptr, "sdl_surface");
 
     f = mod->addFunc(type_SDL_Surface, "cairosdl_surface_get_target",
                      (void *)cairosdl_surface_get_target
                      );
-    f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_cairo_surface_t, "surface");
 
     f = mod->addFunc(type_void, "cairosdl_surface_flush_rects",
                      (void *)cairosdl_surface_flush_rects
                      );
-    f->addArg(type_cairo_surface_t, "surface");
-    f->addArg(type_int, "num_rects");
-    f->addArg(array_pSDL__Rect_q, "rects");
+       f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_int, "num_rects");
+       f->addArg(array_pSDL__Rect_q, "rects");
 
     f = mod->addFunc(type_void, "cairosdl_surface_flush_rect",
                      (void *)cairosdl_surface_flush_rect
                      );
-    f->addArg(type_cairo_surface_t, "surface");
-    f->addArg(type_int, "x");
-    f->addArg(type_int, "y");
-    f->addArg(type_int, "width");
-    f->addArg(type_int, "height");
+       f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_int, "x");
+       f->addArg(type_int, "y");
+       f->addArg(type_int, "width");
+       f->addArg(type_int, "height");
 
     f = mod->addFunc(type_void, "cairosdl_surface_flush",
                      (void *)cairosdl_surface_flush
                      );
-    f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_cairo_surface_t, "surface");
 
     f = mod->addFunc(type_void, "cairosdl_surface_mark_dirty_rects",
                      (void *)cairosdl_surface_mark_dirty_rects
                      );
-    f->addArg(type_cairo_surface_t, "surface");
-    f->addArg(type_int, "num_rects");
-    f->addArg(array_pSDL__Rect_q, "rects");
+       f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_int, "num_rects");
+       f->addArg(array_pSDL__Rect_q, "rects");
 
     f = mod->addFunc(type_void, "cairosdl_surface_mark_dirty_rect",
                      (void *)cairosdl_surface_mark_dirty_rect
                      );
-    f->addArg(type_cairo_surface_t, "surface");
-    f->addArg(type_int, "x");
-    f->addArg(type_int, "y");
-    f->addArg(type_int, "width");
-    f->addArg(type_int, "height");
+       f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_int, "x");
+       f->addArg(type_int, "y");
+       f->addArg(type_int, "width");
+       f->addArg(type_int, "height");
 
     f = mod->addFunc(type_void, "cairosdl_surface_mark_dirty",
                      (void *)cairosdl_surface_mark_dirty
                      );
-    f->addArg(type_cairo_surface_t, "surface");
+       f->addArg(type_cairo_surface_t, "surface");
 
     f = mod->addFunc(type_cairo_t, "cairosdl_create",
                      (void *)cairosdl_create
                      );
-    f->addArg(type_SDL_Surface, "sdl_surface");
+       f->addArg(type_SDL_Surface, "sdl_surface");
 
     f = mod->addFunc(type_SDL_Surface, "cairosdl_get_target",
                      (void *)cairosdl_get_target
                      );
-    f->addArg(type_cairo_t, "cr");
+       f->addArg(type_cairo_t, "cr");
 
     f = mod->addFunc(type_void, "cairosdl_destroy",
                      (void *)cairosdl_destroy
                      );
-    f->addArg(type_cairo_t, "cr");
+       f->addArg(type_cairo_t, "cr");
+
 
     mod->addConstant(type_uint32, "CAIROSDL_ASHIFT",
                      static_cast<int>(24)
