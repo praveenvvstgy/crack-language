a56cbe0fe44c4ac3015dcc337eb3bb5ccb2947dd
mmuller <mmuller@google.com>
2011-07-14 19:50:10 -0400

    Fix direct calls/referencing of static member funcs, emit an error on attempts to reference methods.

---
+++ model/IntConst.h
@@ -0,0 +1,40 @@
+// Copyright 2009 Google Inc.
+
+#ifndef _model_IntConst_h_
+#define _model_IntConst_h_
+
+#include "Context.h"
+#include "Expr.h"
+#include <stdint.h>
+
+namespace model {
+
+SPUG_RCPTR(IntConst);
+
+// There's only one kind of integer constant - it can be specialized based on 
+// context - e.g. 32 bit, 64 bit, signed versus unsigned...
+class IntConst : public Expr {
+    public:
+
+        union {
+             int64_t sval;
+            uint64_t uval;
+        } val;
+
+        bool reqUnsigned; // > INT64_MAX so requires unsigned 64
+
+        IntConst(TypeDef *type, int64_t val);
+        IntConst(TypeDef *type, uint64_t val);
+        
+        virtual ResultExprPtr emit(Context &context);
+        virtual ExprPtr convert(Context &context, TypeDef *newType);
+        virtual void writeTo(std::ostream &out) const;
+        virtual bool isAdaptive() const;
+        
+        /** Return the default type for the value. */
+        static TypeDef *selectType(Context &context, int64_t val);
+};
+
+} // namespace parser
+
+#endif
