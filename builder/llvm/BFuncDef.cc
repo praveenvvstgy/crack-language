0700c24027e9edcab16452a75cb1a1f961bd5ae5
weyrick <weyrick@mozek.us>
2011-08-08 19:30:23 -0400

    Include final terminating null in makePrimArray

---
+++ builder/llvm/BFuncDef.cc
@@ -0,0 +1,33 @@
+// Copyright 2010 Google Inc, Shannon Weyrick <weyrick@mozek.us>
+
+#include "BFuncDef.h"
+#include "model/Context.h"
+#include "builder/llvm/LLVMBuilder.h"
+
+#include <string>
+
+#include <llvm/Function.h>
+
+using namespace builder::mvll;
+
+void BFuncDef::setOwner(model::Namespace *o) {
+    owner = o;
+    fullName.clear();    
+    // if an overridden symbolName isn't set, we use the canonical name
+    if (symbolName.empty())
+        rep->setName(getFullName());
+}
+
+llvm::Function * BFuncDef::getRep(LLVMBuilder &builder) {
+    if (rep->getParent() != builder.module)
+        return builder.getModFunc(this);
+    else
+        return rep;
+}
+
+
+// only used for annotation functions
+void *BFuncDef::getFuncAddr(Builder &builder) {
+    LLVMBuilder &b = dynamic_cast<LLVMBuilder&>(builder);
+    return b.getFuncAddr(getRep(b));
+}
