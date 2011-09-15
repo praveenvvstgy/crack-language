94b6902de9f62b601fdf9b3e5599c5e01ceaed35
Conrad Steenberg <conrad.steenberg@caltech.edu>
2011-09-15 12:03:23 -0700

    Rename file to reflect renaming the class.

---
+++ model/StrConst.h
@@ -0,0 +1,23 @@
+// Copyright 2009 Google Inc.
+
+#ifndef _model_StrConst_h_
+#define _model_StrConst_h_
+    
+#include "Context.h"
+#include "Expr.h"
+
+namespace model {
+
+class StrConst : public Expr {
+    public:
+        std::string val;
+        
+        StrConst(TypeDef *type, const std::string &val);
+        
+        virtual ResultExprPtr emit(Context &context);
+        virtual void writeTo(std::ostream &out) const;
+};
+
+} // namespace parser
+
+#endif
