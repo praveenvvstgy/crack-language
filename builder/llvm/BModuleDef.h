5f0164980cbd30bcb1b8ce7726fc744cc291d482
mmuller <mmuller@enduden.com>
2012-02-21 09:32:45 -0500

    Fix unsigned comparison operators so that they don't do signed comparison.

---
+++ builder/llvm/BModuleDef.h
@@ -0,0 +1,64 @@
+// Copyright 2011 Google Inc, Shannon Weyrick <weyrick@mozek.us>
+
+#ifndef _builder_llvm_BModuleDef_h_
+#define _builder_llvm_BModuleDef_h_
+
+#include "builder/util/SourceDigest.h"
+#include "model/ModuleDef.h"
+#include <spug/RCPtr.h>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace model {
+    class Context;
+}
+
+namespace llvm {
+    class Module;
+}
+
+namespace builder {
+namespace mvll {
+
+SPUG_RCPTR(BModuleDef);
+
+class BModuleDef : public model::ModuleDef {
+
+public:
+    // primitive cleanup function
+    void (*cleanup)();
+    llvm::Module *rep;
+
+    // source text hash code, used for caching
+    SourceDigest digest;
+
+    // list of modules imported by this one, along with its imported symbols
+    typedef std::map<BModuleDef*, std::vector<std::string> > ImportListType;
+    ImportListType importList;
+    // list of shared libraries imported, along with imported symbols
+    typedef std::map<std::string, std::vector<std::string> > ShlibImportListType;
+    ShlibImportListType shlibImportList;
+
+    BModuleDef(const std::string &canonicalName,
+               model::Namespace *parent,
+               llvm::Module *rep0
+               ) :
+            ModuleDef(canonicalName, parent),
+            cleanup(0),
+            rep(rep0),
+            digest(),
+            importList()
+    {
+    }
+
+    void callDestructor() {
+        if (cleanup)
+            cleanup();
+    }
+};
+
+} // end namespace builder::vmll
+} // end namespace builder
+
+#endif
